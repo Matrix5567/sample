009a240686ebfcce21c272fc78c3f6f15ebd2201
kevin <kevinbabuvarkey@gmail.com>
2022-06-10 09:49:47 +0530

    testing

--- superapp/urls.py
+++ superapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	path('create/', views.add_items, name='add-items'),
 	path('all/', views.view_items, name='view_items'),
 	path('update/<int:pk>/', views.update_items, name='update-items'),
-	path('item/<int:pk>/delete/', views.delete_items, name='delete-items'),
+	path('delete/<int:pk>/', views.delete_items, name='delete-items'),
 	path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 	path('basic/', views.API_objects.as_view()),
     path('basic/<int:pk>/', views.API_objects_details.as_view()),
